c6166a528ad7221be1b9e31bbdec143e3c9dc806
mmarifat <mma.rifat66@gmail.com>
2023-03-28 17:57:57 +0100

    - added cors support for send-email

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
 import { getLicenseInfo } from './get-license-info';
 import MongoConnect from '@library/server/mongo';
 import EmailTransporter from '@library/server/email-transporter';
@@ -44,6 +45,11 @@ export const sendEmailHelper = async (key: string, to: string, subject: string, 
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     if (req.method !== 'POST') {
         res.status(400).json({ status: 400, message: 'Wrong request method' });
     }
